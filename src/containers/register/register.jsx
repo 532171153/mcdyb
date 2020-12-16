cfabeea1f7b1b084605694aec326edca0a7eafff
qisyuan <qisiyuan@octopus-staff.com>
2020-12-16 18:38:35 +0800

    Logo组件（未完成）

--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -13,6 +13,7 @@ import {
 } from "antd-mobile";
 
 import Logo from "../../components/logo/logo";
+import "./register.less";
 
 const ListItem = List.Item;
 
@@ -21,7 +22,23 @@ export default class Register extends Component {
 		return (
 			<div>
 				<NavBar>迈出第一步</NavBar>
-				<Logo></Logo>
+				<Logo />
+				<WingBlank>
+					<List>
+						<InputItem>用&nbsp;&nbsp;户&nbsp;&nbsp;名：</InputItem>
+						<InputItem type="password">密&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;码：</InputItem>
+						<InputItem type="password">确认密码：</InputItem>
+						<ListItem>
+							<span>用户类型：</span>
+								<Radio className="typeRadio">大神</Radio>
+								<Radio className="typeRadio">老板</Radio>
+
+						</ListItem>
+						<WhiteSpace />
+						<Button type="primary" className="register">注册</Button>
+						<Button>已有账户</Button>
+					</List>
+				</WingBlank>
 			</div>
 		);
 	}
